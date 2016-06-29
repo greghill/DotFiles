2dfc301343e3698b5a9a65ab12dbe0967f4e27d9
Greg D. Hill <greghill@cs.stanford.edu>
2016-06-29 01:26:20 +0000

    supress error message from linking *

--- initialize.sh
+++ initialize.sh
@@ -3,5 +3,5 @@ cp /etc/default/keyboard ~/.etc_default_keyboard.old
 sudo cp etc_default_keyboard /etc/default/keyboard 
 
 mv ~/.bashrc ~/.bashrc.old
-# link all dot files in this folder to home directory, will error for .. and .git but no worries
-ln .* ~
+# link all dot files in this folder to home directory, will error for . and .. and .git but no worries
+ln .* ~ 2> /dev/null
